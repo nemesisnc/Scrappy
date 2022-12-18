e6cbb3cc47c545d8be0dfeec42bd4d41e9e86259
RM <nemesis.nc@gmail.com>
2022-12-18 23:23:40 +1100

    Converted the test in a library

---
+++ Scrappy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = true,
+        });
+
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://juridoc.gouv.nc/");
+
+        await page.FrameLocator("frame[name=\"Mframe\"]").GetByRole(AriaRole.Link, new() { NameString = "Edition courante et numéros spéciaux" }).ClickAsync();
+
+        await page.FrameLocator("frame[name=\"Mframe\"]").FrameLocator("frame[name=\"Lframe\"]").Locator("#sliderDis1").SelectOptionAsync(new[] { "2022" });
+
+        var download = await page.RunAndWaitForDownloadAsync(async () =>
+        {
+            await page.FrameLocator("frame[name=\"Mframe\"]").FrameLocator("frame[name=\"Lframe\"]").GetByRole(AriaRole.Link, new() { NameString = "Consulter la version complète Pdf du n° 10488 (HTTP)" }).ClickAsync(new LocatorClickOptions
+            {
+                Modifiers = new[] { KeyboardModifier.Alt },
+            });
+        });
+
+        await download.SaveAsAsync("./10488.pdf");
+    }
+}
